746bac6d9bad6a2e05e822304e1cc801feb87b9e
PiñiscoRata <piiscorata@gmail.com>
2021-06-26 01:04:09 -0400

    Sistema de Comentarios

---
+++ src/components/Modal/ModalPublication/ModalPublication.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { Modal, Grid } from 'semantic-ui-react';
+import Comments from './Comments';
+import CommentForm from './CommentForm';
+
+import './ModalPublication.scss';
+
+const ModalPublication = ({ show, setShow, publication }) => {
+
+    const onClose = () => setShow(false);
+    return (
+        <Modal open={show} onClose={onClose} className="modal-publication">
+            <Grid>
+                <Grid.Column 
+                    className="modal-publication__left" 
+                    width={10} 
+                    style={{ backgroundImage: `url("${publication.file}")` }} 
+                />
+
+                <Grid.Column className="modal-publication__right" width={6}>
+                    <Comments publication={publication} />
+                    <div>Actions</div>
+                    <CommentForm publication={publication} />
+                </Grid.Column>
+            </Grid>
+        </Modal>
+    );
+};
+
+export default ModalPublication;
--- src/components/Publications/PreviewPublication/PreviewPublication.js
+++ src/components/Publications/PreviewPublication/PreviewPublication.js
@@ -1,16 +1,21 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import { Image } from 'semantic-ui-react';
+import ModalPublication from './../../Modal/ModalPublication';
 
 import './PreviewPublication.scss';
 
 const PreviewPublication = ({ publication }) => {
 
+    const [showModal, setShowModal] = useState(false);
+
     return (
         <Fragment>
-            <div className="preview-publication">
+            <div className="preview-publication" onClick={() => setShowModal(true)}>
                 <Image className="preview-publication__image" src={publication.file} />
             </div>
+
+            <ModalPublication show={showModal} setShow={setShowModal} publication={publication} />
         </Fragment>
     );
 };
